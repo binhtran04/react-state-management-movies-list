6f1802365b3fc4c8f228a4f44daf66ac1e24d7f1
Binh Tran <thanhbinh.tran93@gmail.com>
2020-03-31 15:26:54 +0300

    Add MoviesContext

---
+++ src/MoviesContext.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import {initialMovies} from './initialState';
+
+const defaultDispatch = () => undefined
+
+const MoviesContext = React.createContext({
+  state: {movies: []},
+  dispatch: defaultDispatch
+})
+const ADD_MOVIE = 'ADD_MOVIE';
+const MARK_WATCHED_MOVIE = 'MARK_WATCHED_MOVIE';
+
+const moviesReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_MOVIE: {
+      const movies = [action.payload, ...state.movies];
+      return {
+        ...state,
+        movies
+      }
+    }
+    case MARK_WATCHED_MOVIE: {
+      const movies = state.movies.map(movie => {
+        if (movie.id === action.payload.id) {
+          return {...movie, watched: !movie.watched};
+        }
+
+        return movie;
+      })
+
+      return {
+        ...state,
+        movies
+      }
+    }
+      
+    default:
+      throw new Error(`Action is not supported: ${action.type}`);
+  }
+}
+
+const initialState = {
+  movies: initialMovies,
+}
+
+
+export const MoviesProvider = (props) => {
+  const [state, dispatch] = React.useReducer(moviesReducer, initialState)
+
+  const value = React.useMemo(() => ({state, dispatch}), [state])
+  
+  return <MoviesContext.Provider value={value} {...props} />
+
+}
+
+export const useMoviesContext = () => {
+  const context = React.useContext(MoviesContext)
+
+  if (!context) {
+    throw new Error('useMoviesContext must be used inside a MoviesProvider')
+  }
+
+  const {state, dispatch} = context;
+  const {movies} = state
+
+  const addMovie = (movie) => {
+    movie.id = uuidv4()
+    movie.watched = false
+    dispatch({type: ADD_MOVIE, payload: movie})
+  }
+
+  const markWatchedMovie = (id) => {
+    dispatch({type: MARK_WATCHED_MOVIE, payload: {id}})
+  }
+
+  return {movies, addMovie, markWatchedMovie}
+}
---
+++ src/initialState.js
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const initialMovies = [
+  {
+    id: uuidv4(),
+    title: 'The Godfather',
+    year: 1972,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Inception',
+    year: 2010,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Fight Club',
+    year: 1999,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Pulp Fiction',
+    year: 1994,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Forrest Gump',
+    year: 1994,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'The Matrix',
+    year: 1999,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Spirited Away',
+    year: 2001,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Interstellar',
+    year: 2014,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'The Usual Suspects',
+    year: 1995,
+    watched: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'The Departed',
+    year: 2006,
+    watched: false,
+  },
+];
